18a4aa11ea0e79ef5d0e6c1199d4ec4980193ad9
Валерий <kaktuz34@gmail.com>
2018-08-21 15:44:53 +0300

    update init for video bg

--- src/js/components/_video.js
+++ src/js/components/_video.js
@@ -1,9 +1,12 @@
 import vide from 'vide';
 
 $(document).ready(function() {
-  const container = $('.js-video video').get(0);
-  if (container && container.paused) {
-    container.play();
-  }
-  $(container).attr('playsinline','');
+  setTimeout(() => {
+    const container = $('.js-video video').get(0);
+    if (container && container.paused) {
+      container.play();
+    }
+    $(container).attr('playsinline','');
+
+  }, 1000);
 });
